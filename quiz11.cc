d0f0ff50a8c4b5d19c053db0d37dfaaa3626fdbc
Keshav-khade <your_email@example.com>
2025-07-14 21:24:40 +0530

    modify this

--- quiz11.cc
+++ quiz11.cc
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <limits.h>
-
+#include <algorithm>  // for max_element, min_element
 using namespace std;
 
 int main(){
 
           // this is for finding maximum element in an array 
-          int arr[7] = {15, 3, 9, 27, 6, -4, 18};
+          int arr[7] = {15, 3, 9, 27, 6, -4, 18}; // array declaration 
+
+
           int max = INT_MIN ;
           for(int i=0;i<7;i++){ 
                     if(arr[i] > max){
@@ -24,5 +26,21 @@ int main(){
                     }
           }
           cout<<"our minimum element is "<<min<<endl;
+
+          // using STL C++ YOU CAN ALSO GET *MAXI AND *MINI
+          int n = sizeof(arr) / sizeof(arr[0]);
+
+          int maxVal = *max_element(arr, arr + n);
+          int minVal = *min_element(arr, arr + n); // always remember it returns iterator you just have to dereference it 
+
+          cout << "Maximum element: " << maxVal << endl;
+          cout << "Minimum element: " << minVal << endl;
+
+          // using sorting technique if array is already not sorted
+          sort(arr, arr + n);
+
+          cout << "Minimum element: " << arr[0] << endl;
+          cout << "Maximum element: " << arr[n - 1] << endl;
+
           return 0;
 }
