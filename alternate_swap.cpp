fa96d926adf01e3eda7baa265f117ec73f1dd4d3
Keshav-khade <your_email@example.com>
2025-07-16 00:03:13 +0530

    file just modified

--- alternate_swap.cpp
+++ alternate_swap.cpp
@@ -8,17 +8,17 @@ void print(int arr[],int size){
 }
 
 //swaping alternative element using prebuild swap function 
-// int main(){
-//           int arr[] = {2,3,4,5,8};
-//           int n = sizeof(arr)/sizeof(arr[0]);
-//           for(int i=0;i<n;i+=2){
-//                     if((i+1) < n){
-//                         swap(arr[i],arr[i+1]);       
-//                     }
-//           }
-//           print(arr,n);
-//           return 0;
-// }
+int main(){
+          int arr[] = {2,3,4,5,8};
+          int n = sizeof(arr)/sizeof(arr[0]);
+          for(int i=0;i<n;i+=2){
+                    if((i+1) < n){ // ensures any undefined behaviour
+                        swap(arr[i],arr[i+1]);       
+                    }
+          }
+          print(arr,n);
+          return 0;
+}
 
 // swapping using manual way or traditional way 
 int main(){
